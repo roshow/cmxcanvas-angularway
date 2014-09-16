0b4defbdf14b0044f88528978be1116c7fb6839b
roshow <rolando@roshow.net>
2014-09-15 23:52:03 -0400

    trying to use bootstrap for the nav

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -88,10 +88,12 @@ angular.module('angularcmxApp')
 
             var $canvasEl = element.find('canvas'),
                 canvasEl = $canvasEl[0];
+            
             var heightDiff = 109;
             var widthDiff = 2;
             var thisHeight = $window.innerHeight - heightDiff;
             var thisWidth = $window.innerWidth - widthDiff;
+
             if (thisWidth/thisHeight >= 16/9){
                 canvasEl.style.zoom = thisHeight/canvasEl.height;
             }
@@ -114,4 +116,81 @@ angular.module('angularcmxApp')
             }
         }
     };
-}]);
+}])
+ .directive('fbLike', [
+        '$window', function($window) {
+            return {
+                restrict: 'A',
+                link: function(scope, element, attrs) {
+                    // wait for facebook api to load before displaying like button
+                    scope.$watch(function() { return !!$window.FB; },
+                        function(fbIsReady) {
+                            if (fbIsReady) {
+                                element.html('<div class="fb-like" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>');
+                                $window.FB.XFBML.parse(element.parent()[0]);
+                            }
+                        });
+                }
+            };
+        }
+    ])
+    .directive('googlePlus', [
+        '$window', function($window) {
+            return {
+                restrict: 'A',
+                link: function(scope, element, attrs) {
+                    // wait for google api to load before displaying plus button
+                    scope.$watch(function() { return !!$window.gapi; },
+                        function(gapiIsReady) {
+                            if (gapiIsReady) {
+                                element.html('<div class="g-plusone" data-size="medium"></div>');
+                                $window.gapi.plusone.go(element.parent()[0]);
+                            }
+                        });
+                }
+            };
+        }
+    ])
+    .directive('pinIt', [
+        '$window', '$location',
+        function($window, $location) {
+            return {
+                restrict: 'A',
+                scope: {
+                    pinIt: '=',
+                    pinItImage: '='
+                },
+                link: function(scope, element, attrs) {
+                    // wait for pinterest api to load and scope data to bind before displaying pin it button
+                    scope.$watch(function() { return !!$window.parsePins && !!scope.pinIt; },
+                        function(pinterestIsReady) {
+                            if (pinterestIsReady) {
+                                scope.pinItUrl = $location.absUrl();
+                                element.html('<a href="//www.pinterest.com/pin/create/button/?url=' + scope.pinItUrl + '&media=' + scope.pinItImage + '&description=' + scope.pinIt + '" data-pin-do="buttonPin" data-pin-config="beside"><img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_gray_20.png" /></a>');
+                                $window.parsePins(element.parent()[0]);
+                            }
+                        });
+                }
+            };
+        }
+    ])
+    .directive('tweet', [
+        '$window', function($window) {
+            return {
+                restrict: 'A',
+                scope: {
+                    tweet: '='
+                },
+                link: function(scope, element, attrs) {
+                    // wait for twitter api to load and scope data to bind before displaying tweet button
+                    scope.$watch(function() { return !!$window.twttr && !!scope.tweet; },
+                        function(twttrIsReady) {
+                            if (twttrIsReady) {
+                                element.html('<a href="https://twitter.com/share" class="twitter-share-button" data-text="' + scope.tweet + '">Tweet</a>');
+                                $window.twttr.widgets.load(element.parent()[0]);
+                            }
+                        });
+                }
+            };
+        }
+    ]);
