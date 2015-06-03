d34a8cd25f9d1a828e420675d2fc5e4f2c11e256
roshow <rolando@roshow.net>
2015-06-02 23:27:54 -0400

    new build

--- dist/index.php
+++ dist/index.php
@@ -9,21 +9,20 @@
     <?php include 'metatags.php' ?>
     <link rel="stylesheet" href="styles/vendor.20833e0b.css">
     
-    <link rel="stylesheet" href="styles/main.ea472ff3.css">
+    <link rel="stylesheet" href="styles/main.d8eb40c0.css">
+
     <script type="text/javascript">var switchTo5x=true;</script>
     <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
-    <script type="text/javascript">stLight.options({
-        publisher: '06ea2066-6c1c-4ec3-8a42-64b3a3399360',
-        shorten:false
-    });
+    <script type="text/javascript">
+        stLight.options({
+            publisher: '06ea2066-6c1c-4ec3-8a42-64b3a3399360',
+            shorten:false
+        });
     </script>
+
   </head>
 
   <body ng-app="angularcmxApp">
-    <script type="text/javascript">
-        // Good old fashion javascript for a random body class.
-        if ( Date.now()%2 === 1 ) { document.querySelector('body').className += ' alt'; }
-    </script>
     
     <div class="appcontainer" ng-view=""></div>
 
@@ -31,7 +30,8 @@
     <script src="scripts/vendor.189dac8d.js"></script>
 
     <script src="scripts/canvasbook.4b9e23b2.js"></script>
-    <script src="scripts/scripts.c4577896.js"></script>
+
+    <script src="scripts/scripts.73531d96.js"></script>
 
 
 </body>
